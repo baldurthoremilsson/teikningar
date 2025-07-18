c7e364704d0f69c4d518df7c6afcb81b7a09beca
Baldur Þór Emilsson <baldur@baldur.biz>
2025-07-18 00:35:01 +0000

    cron-scraper splits addresses

--- scraper/cron-scraper.py
+++ scraper/cron-scraper.py
@@ -9,9 +9,8 @@ import time
 import requests
 from datetime import datetime
 import os
-import shutil
 import hashlib
-from enum import Enum
+import re
 
 logger = logging.getLogger(__name__)
 
@@ -19,6 +18,12 @@ logger = logging.getLogger(__name__)
 PHASE_RESTART = "restart"
 PHASE_FETCH_DESCENDING = "fetch-descending"
 PHASE_FETCH_ASCENDING = "fetch-ascending"
+
+DATE_KEY = "30"
+STREET_NAME_KEY = "203"
+HOUSE_NUMBER_KEY = "204"
+ADDRESS_KEY = "210"
+DESCRIPTION_KEY = "214"
 
 
 def get_next_phase(phase: str) -> str:
@@ -59,7 +64,7 @@ def read_config(configfile: str) -> Config:
     config = Config(
         run_for_seconds=parser.getint("scrape", "run_for_seconds"),
         sleep_milliseconds=parser.getint("scrape", "sleep_milliseconds"),
-        logfile=parser.get("scrape", "logfile"),
+        logfile=parser.get("scrape", "scraper_logfile"),
         data_dir=parser.get("scrape", "data_dir"),
         log_to_stderr=parser.get("scrape", "log_to_stderr", fallback="false"),
     )
@@ -106,11 +111,7 @@ def rename_current_scrape_dir(data_dir, scrape_id):
     os.rename(scrape_dir, last_dir)
 
 
-def append_data(data_dir: str, status: dict, teikningar_data: dict) -> None:
-    address = teikningar_data["address"]
-    assert type(address) == str
-    assert len(address) > 0
-
+def append_data(data_dir: str, status: dict, address: str, img_data: dict) -> None:
     address_file = os.path.join(data_dir, status["scrape_id"], f"{address}.json")
     if os.path.exists(address_file):
         with open(address_file) as f:
@@ -118,26 +119,137 @@ def append_data(data_dir: str, status: dict, teikningar_data: dict) -> None:
     else:
         address_list = []
 
-    address_list.append(teikningar_data)
+    address_list.append(img_data)
 
     with open(address_file, "w") as f:
         json.dump(address_list, f)
 
 
+def single_space(address: str) -> str:
+    return " ".join(part for part in address.split(" ") if part != "")
+
+
+def parse_address(address: str, href: str) -> list[str]:
+    """
+    Check: what's the usual case for 2A or 2a? Uppercase or lowercase?
+    Edge cases:
+    'Básbryggja 19-21 Naustabryggja 24'
+    'Básbryggja 19-21 Naustabryggja 26'
+    'Dragháls 18-26 18'
+    'Dragháls 18-26 20'
+    'Dragháls 18-26 22'
+    'Dragháls 18-26 24'
+    'Dragháls 18-26 26'
+    'Eirhöfði 8 Breiðhöfði 15'
+    'Gullengi 11*'
+    'Hæðargarður 27 A'
+    'Klapparstígur 1- 7'
+    'Laugavegur 22 -Klapparstígur 33'
+    'Ljósaland 1-25 10'
+    'Ljósaland 1-25 12'
+    'Ljósaland 1-25 14'
+    'Ljósaland 1-25 16'
+    'Ljósaland 1-25 18'
+    'Ljósaland 1-25 2'
+    'Ljósaland 1-25 20'
+    'Ljósaland 1-25 22'
+    'Ljósaland 1-25 24'
+    'Ljósaland 1-25 4'
+    'Ljósaland 1-25 6'
+    'Ljósaland 1-25 8'
+    'Naustabryggja 55- 57'
+    'Skeifan 15, Faxafen 8'
+    'Skrauthólar 5 vegsvæði Vesturlandsvegar'
+    'Skrauthólar 5 vegsvæði hliðarvegar'
+    'Smábýli 12 vegsvæði'
+    'Sundahöfn 1.3, 1.4'
+    'Ártún vegsvæði 2 hliðarvegar'
+    'Ártún vegsvæði 3 hliðarvegar'
+
+    'laugavegur-60a': ['Laugavegur 60A', 'Laugavegur 60a']
+    'eddufell-8': ['Eddufell 8', 'eDDUFELL 8']
+    'eddufell-2': ['Eddufell 2', 'eDDUFELL 2']
+    'eddufell-4': ['Eddufell 4', 'eDDUFELL 4']
+    'eddufell-6': ['Eddufell 6', 'eDDUFELL 6']
+    'skipholt-17a': ['Skipholt 17A', 'Skipholt 17a']
+    'kleppsvegur-bensinstoed': ['Kleppsvegur bensínstöð'
+    'Kleppsvegur Bensínstöð']
+    'vesturgata-5b': ['Vesturgata 5B', 'Vesturgata 5b']
+    'korngardar-13a': ['Korngarðar 13A', 'Korngarðar 13a']
+    'ystibaer-9': ['Ystibær 9', 'ySTIBÆR 9']
+    'laekjargata-14a': ['Lækjargata 14a', 'Lækjargata 14A']
+    """
+    if " - " in address:
+        # "Fossháls 13-15 - Dragháls 14-16"
+        addresses = address.split(" - ")
+    elif "/" in address:
+        # "Thorvaldsenstræti 2-6/Aðalstræti 11"
+        addresses = address.split("/")
+    else:
+        addresses = [address]
+
+    return_addresses = set()
+    for addr in addresses:
+        addr_strip = addr.strip()
+        match = re.match("(?P<street_name>.*) (?P<start>\d+)-(?P<end>\d+)", addr_strip)
+        if match:
+            groupdict = match.groupdict()
+            street_name = groupdict["street_name"]
+            if " " in street_name:
+                logger.warning(f"Space found in street name: `{street_name}` {href}")
+            start = int(groupdict["start"])
+            end = int(groupdict["end"])
+            while start <= end:
+                return_addresses.add(single_space(f"{street_name} {start}"))
+                start += 2
+        else:
+            return_addresses.add(single_space(addr_strip))
+
+    return list(return_addresses)
+
+
+def convert_image(img) -> tuple[list[str], dict]:
+    href = img["href"]
+
+    meta = img["metadata"]
+    if meta == {}:
+        logger.warning(f"Missing metadata for {href}")
+        return [], {}
+
+    if ADDRESS_KEY in meta:
+        address = meta[ADDRESS_KEY]["value"].strip()
+    else:
+        street_name = meta[STREET_NAME_KEY]["value"].strip()
+        house_number = meta[HOUSE_NUMBER_KEY]["value"].strip()
+        address = f"{street_name} {house_number}"
+    parsed_addresses = parse_address(address, href)
+    if DESCRIPTION_KEY in meta:
+        description = meta[DESCRIPTION_KEY]["value"].strip()
+    else:
+        description = "[Lýsingu vantar]"
+
+    date = meta[DATE_KEY]["value"].strip() if DATE_KEY in meta else None
+    hash = hashlib.md5(href.encode()).hexdigest()[:7]
+
+    data = {
+        "address": address,
+        "date": date,
+        "description": description,
+        "hash": hash,
+        "images": {preview["size"]: preview for preview in img["previews"]},
+        "originalHref": href,
+    }
+    return parsed_addresses, data
+
+
 def process(data_dir: str, status: dict, data: list, url: str) -> None:
-    for img in data:
+    for i, img in enumerate(data):
         try:
-            teikningar_data = {
-                "address": img["metadata"]["210"]["value"],
-                "date": img["metadata"]["30"]["value"],
-                "description": img["metadata"]["205"]["value"],
-                "hash": hashlib.md5(img["href"].encode()).hexdigest()[:7],
-                "images": {preview["size"]: preview for preview in img["previews"]},
-                "originalHref": img["href"],
-            }
-            append_data(data_dir, status, teikningar_data)
+            addresses, img_data = convert_image(img)
+            for address in addresses:
+                append_data(data_dir, status, address, img_data)
         except Exception:
-            logger.warning(f"Processing error from {url}")
+            logger.warning(f"Processing error {i}: {url}")
             continue
 
 
